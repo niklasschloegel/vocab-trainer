551ebd40330bfc9f6e0c5f455a952e22ab04491f
Niklas Schlögel <niklasschloegel@gmail.com>
2021-10-11 14:37:25 +0200

    improving order of semantics in lesson screen

--- lib/pages/lesson_screen.dart
+++ lib/pages/lesson_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 import 'package:vocab_trainer/models/models.dart';
 import 'package:vocab_trainer/pages/pages.dart';
 import 'package:vocab_trainer/widgets/widgets.dart';
@@ -16,32 +17,45 @@ class LessonScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.background,
       ),
       floatingActionButton: _lesson.filecards.length > 0
-          ? FloatingActionButton(
-              onPressed: () => Navigator.of(context).pushNamed(
-                LearningScreen.routeName,
-                arguments: _lesson,
+          ? Semantics(
+              sortKey: OrdinalSortKey(0),
+              child: Semantics(
+                label: "Lernen starten",
+                button: true,
+                enabled: true,
+                child: ExcludeSemantics(
+                  child: FloatingActionButton(
+                    onPressed: () => Navigator.of(context).pushNamed(
+                      LearningScreen.routeName,
+                      arguments: _lesson,
+                    ),
+                    child: Icon(Icons.play_arrow),
+                  ),
+                ),
               ),
-              child: Icon(Icons.play_arrow),
             )
           : null,
-      body: _lesson.filecards.length > 0
-          ? CustomGridView(
-              itemBuilder: (ctx, i) {
-                final fileCard = _lesson.filecards[i];
-                return FileCardPreviewCard(
-                  key: ValueKey(fileCard.id),
-                  question: fileCard.question,
-                  answer: fileCard.answer,
-                );
-              },
-              itemCount: _lesson.filecards.length,
-            )
-          : Center(
-              child: Text(
-                "No Filecards created yet.",
-                textAlign: TextAlign.center,
+      body: Semantics(
+        sortKey: OrdinalSortKey(1),
+        child: _lesson.filecards.length > 0
+            ? CustomGridView(
+                itemBuilder: (ctx, i) {
+                  final fileCard = _lesson.filecards[i];
+                  return FileCardPreviewCard(
+                    key: ValueKey(fileCard.id),
+                    question: fileCard.question,
+                    answer: fileCard.answer,
+                  );
+                },
+                itemCount: _lesson.filecards.length,
+              )
+            : Center(
+                child: Text(
+                  "No Filecards created yet.",
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
+      ),
     );
   }
 }
